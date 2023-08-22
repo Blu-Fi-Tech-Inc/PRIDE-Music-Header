4d3ed7313d5cf508ef0c8c309caee268cac92f1d
Blu.Fi Tech, Inc <blufitech@gmail.com>
2023-08-21 21:36:38 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Explore <code>PRIDE Music Arcade</code> for Independent Artists.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:3000"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome!
         </a>
       </header>
     </div>
